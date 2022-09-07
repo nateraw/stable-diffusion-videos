19654273d629f1d8e7d5053fdaa9456246ed09ca
nateraw <nxr9266@g.rit.edu>
2022-09-07 16:57:57 -0400

    :bug: fix vae output

--- stable_diffusion_videos/stable_diffusion_pipeline.py
+++ stable_diffusion_videos/stable_diffusion_pipeline.py
@@ -190,7 +190,7 @@ class StableDiffusionPipeline(DiffusionPipeline):
 
         # scale and decode the image latents with vae
         latents = 1 / 0.18215 * latents
-        image = self.vae.decode(latents).sample
+        image = self.vae.decode(latents)
 
         image = (image / 2 + 0.5).clamp(0, 1)
         image = image.cpu().permute(0, 2, 3, 1).numpy()
